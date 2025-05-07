3b75338f74b06c161975b66c80821eeeb459f571
AliGhasemi01 <alighasemic137@gmail.com>
2025-05-07 15:47:57 +0330

    added basic Tests(to be completed)

---
+++ credits/tests/test_credit_requests.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+from credits.models import CreditRequest, TopUpRequest
+
+User = get_user_model()
+
+class CreditRequestTests(TestCase):
+    def setUp(self):
+        self.seller1 = User.objects.create_user(username='seller1', password='testpass')
+        self.seller2 = User.objects.create_user(username='seller2', password='testpass')
+
+    def test_approved_credit_requests_add_credit(self):
+        for _ in range(10):
+            CreditRequest.objects.create(
+                seller=self.seller1,
+                amount=10000,
+                status='APPROVED',
+                processed_at=timezone.now()
+            )
+            self.seller1.credit += 10000
+            self.seller1.save()
+
+        self.seller1.refresh_from_db()
+        self.assertEqual(self.seller1.credit, 100000)
